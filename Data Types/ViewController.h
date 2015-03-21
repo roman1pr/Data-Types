baf053b192f6299e0985ce96b095a7c71d027c71
roman1pr <iroman.pri@gmail.com>
2015-03-21 17:07:32 +0300

    Initial Commit

---
+++ Data Types/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Data Types
+//
+//  Created by Роман Приискалов on 21/03/15.
+//  Copyright (c) 2015 Роман Приискалов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
